f0e23c31466c8037bef042d7958eb12f795af63e
Josue Henrique <josue21henrique@gmail.com>
2024-03-11 22:28:23 -0300

    tentei roberto

--- android/app/src/main/kotlin/com/example/josue_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.josue_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -46,46 +48,84 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Rin Okumura',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Filho bastardo de Satan, odiado por todos em Assiah.'),
-                      ),
-                    ],
-                  ),
-                  Image.asset(
-                    'assets/images/okumura.jpg',
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Container(
+              width: 385.0,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.blue.shade900,
+                    Colors.blue.shade700,
+                    Colors.blue.shade200,
+                    Colors.blue.shade600,
+                    Colors.blue.shade500,
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                )
               ),
-              ListTile(
-                title: Text('info@email.com'),
-                leading: Icon(Icons.email),
+              child: Card(
+                color: Colors.transparent,
+                child: Padding(
+                  padding: const EdgeInsets.all(100.0),
+                ),
               ),
-            ],
-          ),
-          color: Colors.blue,
+            ),
+            Positioned(
+                top: 10,
+                left: 10,
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'Rin Okumura',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            Positioned(
+              top: 60,
+              left: 15,
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Filho bastardo de Satan, odiado por todos em Assiah.',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              child: Image.asset(
+                '../assets/images/okumura.jpg',
+                width: 100.0,
+                height: 100.0,
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                left: 30,
+                child: Row(
+                  children: [
+                    Text(
+                      'rinokumura@assiah.com',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
